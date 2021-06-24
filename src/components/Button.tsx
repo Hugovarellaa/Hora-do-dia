a0917ee9a7b8df7d0cbefb22abfd884656a463f5
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-23 21:49:18 -0300

    Reactjs
    Primeira Aula Completa

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+
+export function Button() {
+
+  const [counter, setCounter] = useState(0);
+
+  function imcrement() {
+    setCounter(counter + 1)
+    console.log(counter)
+  }
+  return (
+    <button onClick={imcrement}> {counter} </button>
+  )
+}
